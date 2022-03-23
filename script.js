2fccd209f4296c571582a346e9b3c1f688b9effc
TomSoerr <tomix2004@gmail.com>
2022-03-23 11:54:11 +0100

    Add basic code to index.html and script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// array with items for the game
+const rockPaperScissorsArray = ['Rock', 'Paper', 'Scissors']
+
+// computer picks random item
+const computerPlay = () => rockPaperScissorsArray[Math.floor(Math.random() * 3)]
